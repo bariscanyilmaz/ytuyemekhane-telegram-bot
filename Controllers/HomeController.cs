8890d803310c5ba016937f70673064d9a13e11fd
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-09-11 11:41:18 +0300

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ytuyemekhane_telegram_bot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HometController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger _logger;
+
+        public HometController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
